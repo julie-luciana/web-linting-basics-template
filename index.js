9f71239fff620ad44c930ce14e872ec384e6914f
Julia Hromada <hromada.julia@gmail.com>
2024-08-14 14:21:22 +0200

    tryied challenge

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
+  const cards = document.querySelectorAll('[data-js=card]');
+  const button = cards.querySelectorAll('[data-js=buttons]');
+  cards.forEach(cards => {
+    const button = cards.querySelector('[data-js="button"]');
+    const link = cards.querySelector('[data-js="link"]');
+    const arrow = cards.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hide');
+      arrow.classList.toggle('rotate');
+    });
+  });
 }
